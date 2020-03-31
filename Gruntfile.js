7daefda61c9d4a6160019cd30363eef169c29395
cosminPruncu <cosmin.pruncu@evozon.com>
2020-03-31 14:45:48 +0300

    First commit

---
+++ Gruntfile.js
@@ -0,0 +1,66 @@
+module.exports = function (grunt){
+    require('load-grunt-tasks')(grunt);
+
+    var config = {};
+
+    grunt.initConfig({
+        concat: {
+            options: {
+                separator: ';',
+                sourceMap: true
+            },
+            js: {
+                files: {
+                    '_ui/js/main.js': [
+                        // JS FILES HERE
+                        '_ui-src/js/index.js'
+                    ],
+                    '_ui/js/vendor.js' : [
+                        // JQUERY AND BOOTSTRAP HERE
+                        'node_modules/jquery/dist/jquery.min.js',
+                        'node_modules/bootstrap/dist/js/bootstrap.min.js'
+                    ]
+                }
+            }
+        },
+
+        sass : {
+            options : {
+                sourceMap: true
+            },
+            dist : {
+                files : {
+                    '_ui/css/main.css' : '_ui-src/sass/main.scss',
+                    '_ui/css/vendor.css' : '_ui-src/sass/vendor.scss'
+                }
+            }
+        },
+
+        watch : {
+            js : {
+                files : [
+                    '_ui-src/js/*.js',
+                    '_ui-src/js/**/*.js'
+                ],
+                tasks : ['concat'],
+                options : {
+                    interrupt : true,
+                }
+            },
+            sass : {
+                files : [
+                    '_ui-src/sass/*.scss',
+                    '_ui-src/sass/**/*.scss',
+                    '_ui-src/sass/**/**/*.scss'
+                ],
+                tasks : ['sass'],
+                options : {
+                    interrupt : true,
+                }
+            }
+        }
+    });
+
+    grunt.registerTask('default', ['sass', 'concat']);
+    grunt.registerTask('build-dist', ['sass', 'concat']);
+}
---
+++ _ui/js/main.js
@@ -0,0 +1,4 @@
+$(document).ready(function(){
+    console.log("jquery");
+});
+//# sourceMappingURL=main.js.map
